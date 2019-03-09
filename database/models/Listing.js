ed59e9987986a816059c8209325719a44d419afd
aElian01 <eliana.sur@gmail.com>
2019-03-08 18:07:08 -0800

    mongoose schemas and models for listings, customers, and reviews

---
+++ database/models/Listing.js
@@ -0,0 +1,26 @@
+/* eslint-disable camelcase */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  _id: { type: Number, required: true },
+  created_at: { type: Date, required: true },
+  description: String,
+  accuracy: Number,
+  communication: Number,
+  cleanliness: Number,
+  location: Number,
+  check_in: Number,
+  value: Number,
+  customer_id: { type: Number, ref: 'Customer', index: true },
+}, { id: false });
+
+const listingSchema = new Schema({
+  _id: { type: Number, required: true },
+  reviews: [reviewSchema],
+}, { id: false });
+
+const Listing = mongoose.model('Listing', listingSchema);
+
+module.exports.Listing = Listing;
+
