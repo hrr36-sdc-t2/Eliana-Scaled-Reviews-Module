ee39d441ed1c7bce4f7bbf43ab71fc4907761db6
aElian01 <eliana.sur@gmail.com>
2019-03-09 23:09:53 -0800

    removed createFakeReview function into separate utility file

--- database/seed_Listing.js
+++ database/seed_Listing.js
@@ -3,12 +3,12 @@ const faker = require('faker');
 const mongodb = require('./mongodb.js');
 const Listing = require('./models/Listing.js').Listing;
 const asyncPool = require('./async_pool_util.js');
+const createFakeReview = require('./seed_util');
 
 // CONNECTION EVENTS
 // When successfully connected seed customers collection and close mongodb connection
 mongoose.connection.on('connected', async function () {
   await seedListingsCollection();
-  console.log(reviewCounter);
   mongoose.connection.close();
 });
 // establish connection
@@ -21,50 +21,16 @@ const CHUNK_SIZE = 1000;
 const CONCURRENCY = 4;
 const TOTAL_CHUNKS = TOTAL_ENTRIES / CHUNK_SIZE;
 
-var reviewCounter = 1;
 var listingCounter = 1;
-
-var createFakeReview = () => ({
-  created_at: faker.date.past(),
-  description: faker.lorem.sentences(),
-  accuracy: faker.random.number({
-    min: 3,
-    max: 5
-  }),
-  communication: faker.random.number({
-    min: 3,
-    max: 5
-  }),
-  cleanliness: faker.random.number({
-    min: 3,
-    max: 5
-  }),
-  location: faker.random.number({
-    min: 3,
-    max: 5
-  }),
-  check_in: faker.random.number({
-    min: 1,
-    max: 5
-  }),
-  value: faker.random.number({
-    min: 2,
-    max: 5
-  }),
-  //3M customers
-  customer_id: faker.random.number({
-    min: 1,
-    max: 3000000
-  }),
-  _id: reviewCounter++
-});
-
+var reviewCounter = 1;
 
 var makeReviewDocuments = function () {
   var fakeReviews = [];
   var randomQuantityOfReviews = Math.floor((Math.random() * 15) + 1);
   for (let i = 0; i < randomQuantityOfReviews; i++) {
-    fakeReviews.push(createFakeReview());
+    var review = createFakeReview();
+    review['_id'] = new mongoose.Types.ObjectId;
+    fakeReviews.push(review);
   }
   return fakeReviews;
 }
