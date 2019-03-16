4d38b165eab54f29effc1e73682bdbb3398457d3
aElian01 <eliana.sur@gmail.com>
2019-03-15 22:05:44 -0700

    typo

--- database/seeds/seed_Listing.js
+++ database/seeds/seed_Listing.js
@@ -64,6 +64,7 @@ const seedListingsCollection = async function (callback) {
     var promise = Listing.collection.insertMany(data);
     var time_end_insert = new Date();
     total_time_insertion += time_end_insert - time_start_insert;
+    chunks_processed++;
     if (chunks_processed % Math.floor(TOTAL_CHUNKS/10) == 0) {
       console.log("" + (new Date()) + ": Processed " + chunks_processed + " / " + TOTAL_CHUNKS + " chunks");
     }
